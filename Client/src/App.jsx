428f999d0d5c0d533e53f12ef09d522b25425251
Xico <francisco100eg@gmail.com>
2023-03-09 18:44:23 -0300

    Feat: Header component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-
+import logo from "./assets/logo.svg";
+import Vortex from "./assets/logo.png";
+import Home from "./pages/Home";
+import Post from "./pages/Post";
 const App = () => {
   return (
-    <div>
-      <h1>React App</h1>
-    </div>
+    <BrowserRouter>
+      <header
+        className="w-full flex
+         justify-between items-center
+          bg-white sm:px-8 px-4
+           border-b border-b-[#e6ebf4]"
+      >
+        <Link to="/" className="flex items-center">
+          <img
+            src={Vortex}
+            alt="Vortex-logo"
+            className="w-20 object-contain mr-4"
+          />
+          <h2 className="ml-4">Powered By: </h2>
+          <img src={logo} alt="logo" className="w-28 object-contain" />
+        </Link>
+        <Link
+          to="/create-post"
+          className="font-inter ml-4 font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+        >
+          Create
+        </Link>
+      </header>
+    </BrowserRouter>
   );
 };
 
