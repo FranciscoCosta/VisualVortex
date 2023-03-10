542a1cd49da1c7268b40a9ba6e0c26b9b9a99c65
Xico <francisco100eg@gmail.com>
2023-03-10 11:56:13 -0300

    Added connection to mongoDb and created routes

--- Server/index.js
+++ Server/index.js
@@ -2,21 +2,32 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello Vortex");
 });
 
 const startServer = async () => {
-  app.listen(8000, () => {
-    console.log("Server is running on port 8000");
-  });
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8000, () => {
+      console.log("Server is running on port 8000");
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
