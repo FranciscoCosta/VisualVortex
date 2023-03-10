4b929320d6c4024e367f822c35ca7bd80ab5998f
Xico <francisco100eg@gmail.com>
2023-03-10 15:51:31 -0300

    Fix: Remove id from showing

--- Client/src/components/Card.jsx
+++ Client/src/components/Card.jsx
@@ -5,7 +5,6 @@ import { downloadImage } from "../utils";
 
 const Card = ({ _id, name, prompt, photo }) => (
   <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
-    <p>{_id}</p>
     <img
       className="w-full h-auto object-cover rounded-xl"
       src={photo}
