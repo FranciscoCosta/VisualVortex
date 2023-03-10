c53b535e78dce284effb90087d72b559a51fa9ca
Xico <francisco100eg@gmail.com>
2023-03-10 11:13:23 -0300

    Feat: Create Post page

--- Client/src/components/FormField.jsx
+++ Client/src/components/FormField.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 
-const FormField = () => {
-  return <div>FormField</div>;
+const FormField = ({
+  label,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
+  return (
+    <div>
+      <div className="flex item-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {label}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="bg-[#ECECF1] text-cs font-semibold py-1 px-2 rounded-[4px] text-black"
+          >
+            Surpreende-me!
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border-gray-300 text-gray-900 focus:ring-[#46449ff] focus:border-[#46449ff] outline-none p-3 block w-full shadow-sm sm:text-sm border rounded-lg"
+      />
+    </div>
+  );
 };
 
 export default FormField;
--- Client/src/pages/Post.jsx
+++ Client/src/pages/Post.jsx
@@ -17,8 +17,14 @@ const Post = () => {
   const [loading, setLoading] = useState(false);
 
   const handleSumbit = () => {};
-  const handleChange = (e) => {};
-  const handleSurpriseMe = () => {};
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt();
+    setForm({ ...form, prompt: randomPrompt });
+  };
+  const generateImage = () => {};
 
   return (
     <section className="max-w-7x1 mx-auto">
@@ -32,24 +38,67 @@ const Post = () => {
       <form className="mt-16 max-w-3x1" onSubmit={handleSumbit}>
         <div className="flex flex-col gap-5">
           <FormField
-            lavbel="Name"
+            label="Name"
             type="text"
             name="name"
             placeholder="Francisco Costa"
             value={form.name}
-            onChange={handleChange}
+            handleChange={handleChange}
           />
 
           <FormField
-            lavbel="Prompt"
+            label="Prompt"
             type="text"
             name="name"
             placeholder="A digital art illustration of a city skyline at night."
             value={form.prompt}
-            onChange={handleChange}
+            handleChange={handleChange}
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? "Criando..." : "Criar"}
+          </button>
+        </div>
+
+        <div className="mt-10">
+          <p className="mt-2 tex-[#666e75] text-[14px]">
+            Quando criares a imagem podes compartilhar com outros na comunidade.
+          </p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] fonr-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? "Carregando ..." : "Compartilhar"}
+          </button>
         </div>
       </form>
     </section>
