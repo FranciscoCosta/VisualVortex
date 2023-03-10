3c42c2287e6d755ef26c9c0a5e8d66376b975866
Xico <francisco100eg@gmail.com>
2023-03-10 11:38:04 -0300

    Feat: Started the server

---
+++ Server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", (req, res) => {
+  res.send("Hello Vortex");
+});
+
+const startServer = async () => {
+  app.listen(8000, () => {
+    console.log("Server is running on port 8000");
+  });
+};
+
+startServer();
