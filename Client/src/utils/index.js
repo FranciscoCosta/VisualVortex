799d378e6b1587f94353c862a0926f09c17169d0
Xico <francisco100eg@gmail.com>
2023-03-10 15:22:07 -0300

    Feat: Added Cards componenet

--- Client/src/utils/index.js
+++ Client/src/utils/index.js
@@ -1,5 +1,11 @@
+import { FileSaver } from "file-saver";
 import { supriseMePromps } from "../constants/index.js";
 
 export function getRandomPrompt() {
   return supriseMePromps[Math.floor(Math.random() * supriseMePromps.length)];
+}
+
+export async function downloadImage(_id, photo) {
+  console.log(__id, photo);
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
 }
